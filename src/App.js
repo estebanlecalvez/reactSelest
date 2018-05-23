335d0e131b62f806aa7e63c331ee3d61a8d9fa00
Esteban Le Calvez <Esteban>
2018-05-23 10:51:11 +0200

    Ajout du scss + compilation automatique + modification modal inscription/ connexion.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import './css/global.css';
+import './css/main.css';
 
 
 export default class App extends Component {
