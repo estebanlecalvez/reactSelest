e3ee303f1ed6f5158b9d57349fa354b0bd204a88
David Tofigh <david.tofigh7@gmail.com>
2018-06-07 09:42:49 +0200

    modifi scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,36 +4,26 @@ export default class Footer extends Component {
     render() {
         return (
             <div>
-                <div className="footer">
+                <div className="footer-selest">
                     <div className="container">
-                        <div className="footer_grids">
-                            <h2 className="title-footer"></h2>
-                            <div className="block-footer col-md-4 col-sm-6">
-                                <ul className="list1">
-                                    <li><a title="réseau social" href="#">A propos</a></li>
-                                    <li><a title="comment utiliser" href="#">Conditions d'utilisation</a></li>
-                                    <li><a title="confidentialité des données" href="#">Confidentialité</a></li>
-                                    <li><a title="contact" href="#">Nous contacter</a></li>
-                                    <li><a title="aide" href="#">Aide</a></li>
-                                </ul>
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <h3>A propos de Sel’est</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
                             </div>
-                            <div className="block-footer col-md-4 col-sm-6">
+                            <div className="col-sm-4">
                                 <ul className="list2">
-                                    <li><a title="réseau social" href="#">A propos</a></li>
-                                    <li><a title="comment utiliser" href="#">Conditions d'utilisation</a></li>
-                                    <li><a title="confidentialité des données" href="#">Confidentialité</a></li>
-                                    <li><a title="contact" href="#">Nous contacter</a></li>
-                                    <li><a title="aide" href="#">Aide</a></li>
+                                    <li><a title="réseau social" href="#">Demandes</a></li>
+                                    <li><a title="comment utiliser" href="#">Offres</a></li>
+                                    <li><a title="confidentialité des données" href="#">Comment ça marche?</a></li>
+                                    <li><a title="contact" href="#">Aide</a></li>
                                 </ul>
                             </div>
-                            <div className="block-footer col-md-4 col-sm-6">
-                                <ul className="list3">
-                                    <li><a title="réseau social" href="#">A propos</a></li>
-                                    <li><a title="comment utiliser" href="#">Conditions d'utilisation</a></li>
-                                    <li><a title="confidentialité des données" href="#">Confidentialité</a></li>
-                                    <li><a title="contact" href="#">Nous contacter</a></li>
-                                    <li><a title="aide" href="#">Aide</a></li>
-                                </ul>
+                            <div className="col-sm-4">
+                                <h3>Contactez-Nous</h3>
+                                <p>Tel :</p>
+                                <p>Mail :</p>
+                                <p>Adresse :</p>
                             </div>
                         </div>
                     </div>
