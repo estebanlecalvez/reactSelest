d0ff28226f11b6a7d3d55218bc3af9e5fb0bc167
David Tofigh <david.tofigh7@gmail.com>
2018-06-07 14:20:32 +0200

    modification fonts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,46 +1,57 @@
 import React, { Component } from 'react';
 import FbLogo from '../img/theme/fb.png';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Aide from '../components/Aide';
+import HowItWorks from '../components/HowItWorks';
+import Charte from '../pdf/charte.pdf';
+import ReglementInterieur from '../pdf/reglement_interieur.pdf';
 
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                <div className="footer-selest">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-sm-4">
-                                <h3>A propos de Sel’est</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec.</p>
-                            </div>
-                            <div className="col-sm-4 menu-footer">
-                                <ul className="list2">
-                                    <li><a title="réseau social" href="#">Demandes</a></li>
-                                    <li><a title="comment utiliser" href="#">Offres</a></li>
-                                    <li><a title="confidentialité des données" href="#">Comment ça marche?</a></li>
-                                    <li><a title="contact" href="#">Aide</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-sm-4">
-                                <h3>Contactez-Nous</h3>
-                                <p> Tel : 0987654321<br />
-                                    Mail : selest@gmail.com<br />
-                                    Adresse : Bretagne, Vitré</p>
-                                <div className="reseaux-sociaux-selest">
-                                    <ul>
-                                        <li>
-                                            <a href="https://www.facebook.com/selest35/" ><img src={FbLogo} className="small-icon zoom-hover" /></a>
-                                        </li>
+            <Router>
+                <div>
+                    <div className="footer-selest">
+                        <div className="footer-header"></div>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-sm-5">
+                                    <h3 className="zoom-hover">A propos du Sel’est 2.0</h3>
+                                    <p>Le Sel'est Vitré a pour finalité de proposer des services à la personne et de valoriser l’entraide sociale entre individus.<br />
+                                    Elle dispose d’une monnaie virtuelle fictive – Souèt – unique.<br />
+                                    La gestion des souèts est facilitée.Le but de ce site est de mettre en lumière l'association en augmentant leur notoriété.<br />
+                                    La digitalisation de cette activité est utile en vue des mutations technologiques et de l’évolution des attentes et demandes des utilisateurs.</p>
+                                </div>
+                                <div className="col-sm-3 menu">
+                                    <ul className="links">
+                                        <li><a title="réseau social" href="https://www.facebook.com/selest35/">Facebook</a></li>
+                                        <li><Link to="/comment_ça_marche"><a title="comment utiliser">Comment ça marche?</a></Link></li>
+                                        <li><a href={Charte}>Charte</a></li>
+                                        <li><a href={ReglementInterieur}>Reglement interieur</a></li>
                                     </ul>
+                                </div>
+                                <div className="col-sm-3">
+                                    <h3 className="zoom-hover">Contactez-Nous</h3>
+                                    <p> Tel : 0987654321<br />
+                                        Mail : selest@gmail.com<br />
+                                        Adresse : Bretagne, Vitré</p>
+                                    <div className="reseaux-sociaux-selest">
+                                        <ul>
+                                            <li>
+                                                <a href="https://www.facebook.com/selest35/" ><img src={FbLogo} className="small-icon zoom-hover" /></a>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div className="pied-page">
+                        <p>Réalisé par MyDigitalSchool</p>
+                    </div>
+                    <Route path="/comment_ca_marche" component={HowItWorks} />
                 </div>
-                <div className="pied-page">
-                    <p>Réalisé par group MyDigitalSchool</p>
-                </div>
-            </div>
+            </Router>
         );
     }
 }
-
